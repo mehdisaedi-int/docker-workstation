6a1da980e24ccd99d274cef7b6ed8d776aa105dd
Hossein <hosseinakhlaghpour@gmail.com>
2020-10-03 20:41:10 +0000

    adding some config and explanation

--- config/set_ubuntu_display_for_x11.sh
+++ config/set_ubuntu_display_for_x11.sh
@@ -2,6 +2,7 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+xhost +
 # in docker-compose include the following
 #      DISPLAY: $DISPLAY # for mac DISPLAY=docker.for.mac.host.internal:0 # --before running docker-compose and after running xhost +
 #      XAUTHORITY: /tmp/.docker.xauth
@@ -9,3 +10,5 @@ xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 #      - /tmp/.X11-unix:/tmp/.X11-unix
 #      - /tmp/.docker.xauth:/tmp/.docker.xauth
 
+#after starting the container run
+#docker exec -it -u developer unity bash
