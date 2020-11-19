638d3a07a30d27ddad3acda4b548cf2b7baebe0d
Hossein AKhlaghpour <hosseinakhlaghpour@gmail.com>
2020-11-19 18:28:24 +0000

    robo-gym-melodic

--- Ubuntu-18.04/UR10_simulator.sh
+++ Ubuntu-18.04/UR10_simulator.sh
@@ -1,2 +1,3 @@
 . /opt/robogym_ws/devel/setup.bash
 roslaunch ur_robot_server ur10_sim_robot_server.launch gui:=true
+roslaunch ur_robot_server ur10_sim_robot_server.launch         max_velocity_scale_factor:=0.2         action_cycle_rate:=10'
--- Ubuntu-18.04/install.sh
+++ Ubuntu-18.04/install.sh
@@ -7,10 +7,6 @@ git clone -b melodic https://github.com/jr-robotics/mir_robot.git
 git clone -b melodic https://github.com/jr-robotics/universal_robot.git
 
 cd ..
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -qq -y python-rosdep python-catkin-tools
 sudo rosdep init
 rosdep update
 rosdep install --from-paths src -i -y --rosdistro melodic
